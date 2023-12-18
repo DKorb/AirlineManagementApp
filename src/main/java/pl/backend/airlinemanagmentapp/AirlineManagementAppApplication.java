1aff478f870fa1dd7e29d5780c2036927bc9cd7a
Dawid Korbecki <dawid.korbecki@proton.me>
2023-12-18 16:05:27 +0100

    Added dotenv-java library for storing environment variables

--- src/main/java/pl/backend/airlinemanagmentapp/AirlineManagementAppApplication.java
+++ src/main/java/pl/backend/airlinemanagmentapp/AirlineManagementAppApplication.java
@@ -1,5 +1,6 @@
 package pl.backend.airlinemanagmentapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AirlineManagementAppApplication {
 
 	public static void main(String[] args) {
+		// ustawienie zmiennych środowiskowych przez bilbioteke https://github.com/cdimascio/dotenv-java
+		var dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("MYSQLDB_DATABASE", dotenv.get("MYSQLDB_DATABASE"));
+		System.setProperty("MYSQLDB_LOCAL_PORT", dotenv.get("MYSQLDB_LOCAL_PORT"));
+
 		SpringApplication.run(AirlineManagementAppApplication.class, args);
 	}
 
