0f029c760a2a6091e197ae371f9cc5c3f8f395a5
Dawid Korbecki <dawid.korbecki@proton.me>
2023-12-19 22:17:05 +0100

    Updated file service now uses thymeleaf template to generate tickets

--- src/main/java/pl/backend/airlinemanagmentapp/file/FileService.java
+++ src/main/java/pl/backend/airlinemanagmentapp/file/FileService.java
@@ -1,52 +1,39 @@
 package pl.backend.airlinemanagmentapp.file;
 
-import com.itextpdf.barcodes.BarcodeQRCode;
-import com.itextpdf.io.image.ImageData;
-import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.io.util.StreamUtil;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Cell;
-import com.itextpdf.layout.element.Image;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import pl.backend.airlinemanagmentapp.ticket.dto.TicketResponseDTO;
+
 import java.io.ByteArrayOutputStream;
+
+import org.thymeleaf.TemplateEngine;
+
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
-    private final ResourceLoader resourceLoader;
+    private final TemplateEngine templateEngine;
+
 
     public byte[] generateTicketPdf(TicketResponseDTO ticket) {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PdfWriter writer = new PdfWriter(byteArrayOutputStream);
-        PdfDocument pdf = new PdfDocument(writer);
-        Document document = new Document(pdf);
+        var thymeleafContext = new Context();
+        thymeleafContext.setVariable("ticket", ticket);
 
-        Table table = new Table(2);
-        table.addCell(new Cell().add(new Paragraph("Passenger e-mail: ")));
-        table.addCell(new Cell().add(new Paragraph(ticket.user().username())));
-        table.addCell(new Cell().add(new Paragraph("Flight Number: ")));
-        table.addCell(new Cell().add(new Paragraph(ticket.flight().flightNumber())));
-        document.add(table);
+        String htmlContent = templateEngine.process("ticketTemplate", thymeleafContext);
 
-        BarcodeQRCode qrCode = new BarcodeQRCode("ticket information");
-        Image qrCodeImage = new Image(qrCode.createFormXObject(pdf));
-        document.add(qrCodeImage);
+        var byteArrayOutputStream = new ByteArrayOutputStream();
 
-/*        Resource resource = resourceLoader.getResource("classpath:static/airplane-vector.png");
-        ImageData imageData = ImageDataFactory.create(StreamUtil.inputStreamToArray(resource.getInputStream()));
+        var converterProperties = new ConverterProperties();
+        var fontProvider = new DefaultFontProvider(true, true, true);
+        converterProperties.setFontProvider(fontProvider);
 
-        Image img = new Image(imageData);
-        document.add(img);*/
+        HtmlConverter.convertToPdf(htmlContent, byteArrayOutputStream, converterProperties);
 
-        document.close();
         return byteArrayOutputStream.toByteArray();
     }
 
